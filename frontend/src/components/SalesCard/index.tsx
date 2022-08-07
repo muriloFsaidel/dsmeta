442cb3c22a012a904ba1827a46db28b6cde77952
muriloFsaidel <murilofernandessaidel@hotmail.com>
2022-08-07 18:27:05 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 import NotificationButton from "../NotificationButton";
 import './styles.css';
 
@@ -13,15 +15,19 @@ function SalesCard() {
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
      //current Date
     const max = new Date();
-
+    
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    //creating state of the type Sale array getting started with an empty list
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
         //axios, make the request for backend in route sales and then with the response in hands execute this instruction set
-        axios.get("http://localhost:8080/sales")
+        axios.get(`${BASE_URL}/sales`)
         .then(response => {
-            console.log(response.data);
+            //updating list with sales objects
+            setSales(response.data.content);
         })
     },[]);
 
@@ -62,50 +68,31 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
+                        {   //for each sale object in sales array
+                            sales.map(sale =>{
+                                return (
+                                    //render a line and its data columns with the attributes of sale,
+                                    //identifying each line with a key
+                                    <tr key={sale.id}>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dsmeta-red-btn-container">
+                                                <NotificationButton />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                            
+                        }
                     </tbody>
-
                 </table>
             </div>
-
         </div>
     )
 }
---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,2 @@
+// the constant BASE_URL receives the value of env variable VITE_BACKEND_URL if it's not filled ??, BASE_URL by default receives "http://localhost:8080"
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ??  "http://localhost:8080";
